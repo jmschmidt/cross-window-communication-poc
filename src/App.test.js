5bfe7c503bade66a7336ce25e7a27036570e8d9d
Matthew Miller <kale@iammiller.com>
2018-07-13 17:10:53 -0700

    Added SASS, Enzyme, React Router support
    - Updated index.html <title>
    - Added test setup
    - Added .eslintrc
    - Added .editorconfig

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { render } from 'react-testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('renders welcome message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome to React')).toBeInTheDOM();
 });

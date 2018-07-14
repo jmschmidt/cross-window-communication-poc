0f0fe654a9c62676551cd40fd27d968468ddb336
Matthew Miller <kale@iammiller.com>
2018-07-13 17:23:58 -0700

    Added a basic routing example to App
    - Renamed existing .css files to .scss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import Home from './views/home';
+import About from './views/about';
+
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
-    );
-  }
-}
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/views/home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="Home">
+    <h2>Home</h2>
+  </div>
+);
+
+export default Home;

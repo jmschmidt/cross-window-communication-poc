174180687a9a463252ca1c0876c2aa66c898585b
Joseph Schmidt <joseph@dhapdigital.com>
2018-07-16 16:01:04 -0700

    Some minor styling to primary nav component

--- src/components/PrimaryNav/index.js
+++ src/components/PrimaryNav/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import './PrimaryNav.css';
 
 class PrimaryNav extends Component {
   render() {
     return (
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/examples">Examples</Link></li>
-        <li><Link to="/this-is-not-a-valid-url">404 Page</Link></li>
+      <ul className="primary-nav">
+        <li className="primary-nav__item"><Link to="/">Home</Link></li>
+        <li className="primary-nav__item"><Link to="/about">About</Link></li>
+        <li className="primary-nav__item"><Link to="/examples">Examples</Link></li>
+        <li className="primary-nav__item"><Link to="/this-is-not-a-valid-url">404 Page</Link></li>
       </ul>
     );
   }

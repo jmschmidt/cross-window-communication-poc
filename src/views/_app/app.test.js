3930166e037b64a2625677874957b6017f50b446
Matthew Miller <kale@iammiller.com>
2018-07-18 11:22:22 -0700

    Don’t need to mock components with shallow()

--- src/views/_app/app.test.js
+++ src/views/_app/app.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { render } from 'react-testing-library';
 import { App } from '.';
-jest.mock('components/header');
 
 it('renders without crashing', () => {
   shallow(<App t={jest.fn()}/>);

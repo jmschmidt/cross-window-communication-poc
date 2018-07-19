c1c2561cf0ee9a1293bd4f2b2fd255c0248bb84f
Matthew Miller <matthew@millerti.me>
2018-07-19 11:35:41 -0700

    Added rootStore mock to app.test.js

--- src/views/_app/app.test.js
+++ src/views/_app/app.test.js
@@ -3,6 +3,11 @@ import { shallow } from 'enzyme';
 import { render } from 'react-testing-library';
 import { App } from '.';
 
+const rootStoreMock = {
+  uiStore: {},
+  appStore: {},
+}
+
 it('renders without crashing', () => {
-  shallow(<App t={jest.fn()}/>);
+  shallow(<App t={jest.fn()} rootStore={rootStoreMock} />);
 });
